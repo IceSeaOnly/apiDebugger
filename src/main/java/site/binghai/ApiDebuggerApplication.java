1df65b5730cac5089340d2baa9b9f46a3126b0ee
icesea <binghai@meili-inc.com>
2017-09-09 13:18:38 +0800

    Initial commit

---
+++ src/main/java/site/binghai/ApiDebuggerApplication.java
@@ -0,0 +1,12 @@
+package site.binghai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDebuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDebuggerApplication.class, args);
+	}
+}
