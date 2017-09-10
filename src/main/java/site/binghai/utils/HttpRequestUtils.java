60a40376b6eff8363e136fd00d694b97facae731
icesea <binghai@meili-inc.com>
2017-09-10 15:49:18 +0800

    获取请求参数列表兼容post和get

--- src/main/java/site/binghai/utils/HttpRequestUtils.java
+++ src/main/java/site/binghai/utils/HttpRequestUtils.java
@@ -1,6 +1,7 @@
 package site.binghai.utils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 /**
  * Created by binghai on 2017/9/9.
@@ -8,20 +9,30 @@ import javax.servlet.http.HttpServletRequest;
  * @ MoGuJie
  */
 public class HttpRequestUtils {
-    public static String getRequestParams(HttpServletRequest request){
-        return request.getQueryString();
+    public static String getRequestParams(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        Enumeration enu = request.getParameterNames();
+        while (enu.hasMoreElements()) {
+            String paraName = (String) enu.nextElement();
+            if (sb.length() == 0) {
+                sb.append(paraName + "=" + request.getParameter(paraName));
+            } else {
+                sb.append("&" + paraName + "=" + request.getParameter(paraName));
+            }
+        }
+        return sb.toString();
     }
 
-    public static String getRequestURL(HttpServletRequest request){
+    public static String getRequestURL(HttpServletRequest request) {
         return request.getRequestURL().toString();
     }
 
-    public static String getRequestFullPath(HttpServletRequest request){
-        return request.getRequestURL()+"?" + request.getQueryString();
+    public static String getRequestFullPath(HttpServletRequest request) {
+        return getRequestURL(request) + "?" + getRequestParams(request);
     }
 
     public static String getRequestPath(HttpServletRequest request) {
         String full = getRequestFullPath(request);
-        return full.substring(full.lastIndexOf("/"),full.length());
+        return full.substring(full.lastIndexOf("/"), full.length());
     }
 }
